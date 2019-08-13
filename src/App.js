bc8fddbb088c4d4d7655ec190625700945ba27b1
Sandor Nagy <sandor.nagay@dukstra.com>
2019-08-14 00:31:05 +0200

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router';
-import { store } from './redux/store';
-import { reduxConstants } from './redux';
 
 import Home from './pages/Home';
 import Chart from './pages/Chart';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    store.dispatch({
-      type: reduxConstants.SET_URL_SEARCH_STRING,
-      urlSearchString: props.location.search
-    });
-  }
-
   render() {
     return (
       <React.Fragment>
