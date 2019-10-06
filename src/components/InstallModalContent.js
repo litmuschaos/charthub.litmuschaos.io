5894d2d15d8835f149bb8574e7ac11d7fa8f32d3
Pat O'Connor <patoconnor43@gmail.com>
2019-10-05 22:47:38 -0700

    Add copy to clipboard to install modal
    Signed-off-by: Pat O'Connor <patoconnor43@gmail.com>

--- src/components/InstallModalContent.js
+++ src/components/InstallModalContent.js
@@ -1,36 +1,92 @@
-import * as React from 'react';
+import * as React from "react";
 
 export class InstallModalContent extends React.Component {
-
-  constructor (props) {
+  constructor(props) {
     super();
-    this.state = 
-      {value:'', copied:false};
-      
-    
+    this.state = { value: "", copied: false };
+  }
+
+  copyToClipboard = e => {
+    console.log("got it");
+    var textField = document.createElement("textarea");
+    textField.innerText = this.command();
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+    e.target.focus();
+    setTimeout(
+      function() {
+        this.setState({ copied: false });
+      }.bind(this),
+      5000
+    );
+    this.setState({ copied: true });
+  };
+
+  command() {
+    return `kubectl create -f ${this.props.expcrdurl}`;
   }
 
   render() {
     return (
       <div className="install-modal-content">
         <div className="modal-header">
-          <div className="modal-img-placeholder"> <img src={this.props.logo} /></div>
+          <div className="modal-img-placeholder">
+            {" "}
+            <img src={this.props.logo} />
+          </div>
           <div className="modal-header-title-container">
             <h1 className="modal-title">{this.props.displayName}</h1>
-            <span className="modal-subtitle">Provided by {this.props.provider}</span>
+            <span className="modal-subtitle">
+              Provided by {this.props.provider}
+            </span>
           </div>
         </div>
 
         <h3 className="install-title">Install the Chaos Experiments</h3>
 
-        <div className="modal-install-instruction"><span className="modal-install-instruction-number"></span>You can install the Chaos Experiments by following command</div>
-        <div className="modal-code">kubectl create -f  {this.props.expcrdurl}</div>
-        <div className="modal-install-instruction"><span className="modal-install-instruction-number gray-text-color">Notes:</span></div>
+        <div className="modal-install-instruction">
+          <span className="modal-install-instruction-number"></span>You can
+          install the Chaos Experiments by following command
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <div className="modal-code">{this.command()}</div>
+          {document.queryCommandSupported("copy") && (
+            <div>
+              {!this.state.copied && (
+                <button
+                  onClick={this.copyToClipboard}
+                  style={{ padding: 20, background: "none", border: "none" }}
+                >
+                  <i className="mi-clipboard-pen"></i>
+                </button>
+              )}
+              {this.state.copied && <span>Copied</span>}
+            </div>
+          )}
+        </div>
+        <div className="modal-install-instruction">
+          <span className="modal-install-instruction-number gray-text-color">
+            Notes:
+          </span>
+        </div>
 
-        <div className="modal-install-instruction"><span className="modal-install-instruction-number"></span><a href="https://github.com/litmuschaos/chaos-operator/blob/master/README.md">Install Litmus Operator, </a>a tool for for injecting chaos experiments on Kubernetes</div>
+        <div className="modal-install-instruction">
+          <span className="modal-install-instruction-number"></span>
+          <a href="https://github.com/litmuschaos/chaos-operator/blob/master/README.md">
+            Install Litmus Operator,{" "}
+          </a>
+          a tool for for injecting chaos experiments on Kubernetes
+        </div>
         {/* <div>       <a href="https://github.com/litmuschaos/chaos-operator/blob/master/README.md"> https://github.com/litmuschaos/chaos-operator/blob/master/README.md </a></div> */}
-
       </div>
-    )
+    );
   }
 }
