5cacecdec375c8fea042aa31ca6e743376fdc84d
daitya109 <adikid1996@gmail.com>
2019-10-22 22:11:01 +0530

    (refactoring) <area/charthub> : incorporation of some conditions for the git-ops
    Signed-off-by: daitya109 <adikid1996@gmail.com>

--- gitwork/gitwork.go
+++ gitwork/gitwork.go
@@ -4,38 +4,37 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"gopkg.in/src-d/go-git.v4"
 	. "gopkg.in/src-d/go-git.v4/_examples"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 var repoPath = os.Getenv("GOPATH") + "/src/github.com/litmuschaos/chaos-charts/"
+var tempCommit string
+var masterCommit string
 
 func triggerCloning() {
 
 	fmt.Println("triggerClone() -->")
 
 	Info("git clone https://github.com/litmuschaos/chaos-charts")
-	_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
+	r, err := git.PlainClone(repoPath, false, &git.CloneOptions{
 		URL:      "https://github.com/litmuschaos/chaos-charts",
 		Progress: os.Stdout,
 	})
+
+	// Retrieve the branch pointed by HEAD
 	CheckIfError(err)
+	Info("git rev-parse HEAD")
+
+	t, err := r.Head()
+	CheckIfError(err)
+	tempCommit = strings.Split(t.String(), " ")[0]
 
 }
-
-func exists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
-}
-
 func triggerUpdate() {
 
 	fmt.Println("triggerUpdate() -->")
@@ -53,8 +52,80 @@ func triggerUpdate() {
 	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
 	// CheckIfError(err)
 
+	// Retrieve the branch pointed by HEAD
+	Info("git rev-parse HEAD")
+	t, err := r.Head()
+	CheckIfError(err)
+	tempCommit = strings.Split(t.String(), " ")[0]
+
+}
+func triggerHardReset() {
+	fmt.Println("triggerHardReset() -->")
+	// Opens an already existing repository.
+	r, err := git.PlainOpen(repoPath)
+	CheckIfError(err)
+
+	w, err := r.Worktree()
+	CheckIfError(err)
+
+	Info("git reset --hard")
+
+	err = w.Reset(&git.ResetOptions{Mode: git.HardReset})
+
+}
+func triggerGetStatus() bool {
+	fmt.Println("triggerGetStatus() -->")
+	// Opens an already existing repository.
+	r, err := git.PlainOpen(repoPath)
+	CheckIfError(err)
+
+	w, err := r.Worktree()
+	CheckIfError(err)
+
+	// We can verify the current status of the worktree using the method Status.
+	Info("git status --porcelain")
+	status, err := w.Status()
+	CheckIfError(err)
+
+	var listOfFilesChanged []string
+	// fmt.Println(status == nil)
+	for file := range status {
+		listOfFilesChanged = append(listOfFilesChanged, file)
+	}
+	if len(listOfFilesChanged) == 0 {
+		return false //==> clean
+	}
+	return true //==> dirty
+
 }
 
+//commit local commit and global repo
+func compareCommit() bool {
+
+	r, err := git.PlainOpen(repoPath)
+	CheckIfError(err)
+
+	Info("git rev-parse master")
+	h, err := r.ResolveRevision(plumbing.Revision("master"))
+	CheckIfError(err)
+	masterCommit = h.String()
+	fmt.Println(tempCommit)
+	fmt.Println(masterCommit)
+	if masterCommit == tempCommit {
+		return true
+	}
+	return false
+}
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
 func CheckIfError(err error) {
 
 	if err != nil {
@@ -67,13 +138,23 @@ func driver() {
 
 	existenceValue, err := exists(repoPath)
 	CheckIfError(err)
-	fmt.Println("trigger working")
 	if !existenceValue {
 		triggerCloning()
-
 	} else {
-		triggerUpdate()
 
+		if !triggerGetStatus() {
+			// for clean status
+			if !compareCommit() {
+				triggerUpdate()
+			}
+		} else {
+			// for dirty status
+			triggerHardReset()
+			if !compareCommit() {
+				triggerUpdate()
+			}
+
+		}
 	}
 
 }
@@ -83,4 +164,5 @@ func main() {
 		go driver()
 		time.Sleep(6 * time.Hour)
 	}
+
 }
