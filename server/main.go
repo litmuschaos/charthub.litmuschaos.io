1a79c957f7a72cb2ea7ffdf02cdde0afb51f9d0f
daitya109 <adikid1996@gmail.com>
2019-10-24 22:41:33 +0530

    (refactoring)<area/charthub>:dependency-management and packaging of the server directory
    Signed-off-by: daitya109 <adikid1996@gmail.com>

--- server/main.go
+++ server/main.go
@@ -9,7 +9,6 @@ func main() {
 
 	// Trigger is go-routine which synchronously calls the git-ops function Trigger()
 	go Trigger()
-
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
