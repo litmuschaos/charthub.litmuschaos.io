dd326ceb8b039298f5995df0c332fe70719bf68a
SHUBHENDU SHUBHAM <shubhendushubham98@gmail.com>
2019-10-26 15:18:01 +0530

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -6,7 +6,6 @@ process.env.NODE_ENV = 'production';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
   throw err;
 });
