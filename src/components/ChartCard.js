8c5764ccae61d001af548c80afab036871f10f00
ashishjain <ashish.jain@mayadata.io>
2019-12-30 19:17:58 +0530

    fixed text spell
    Signed-off-by: ashishjain <ashish.jain@mayadata.io>

--- src/components/ChartCard.js
+++ src/components/ChartCard.js
@@ -52,8 +52,8 @@ export class ChartCard extends React.Component {
           (<span className="experiment-chaos-type" >
              {this.props.chaosType ?( <i className ="mi-application infra-icon">
              <span className="chaos-tooltiptext ">
-                    { this.props.chartType=="generic" ? ('Intra-Level :- Multiple application might be impact')
-                                        :'Intra-Level :-  Multiple volume sharing the same pool might be impact' }
+                    { this.props.chartType=="generic" ? ('Intra-Level :- Multiple application might be impacted')
+                                        :'Intra-Level :-  Multiple volumes sharing the same pool might be impacted' }
              </span>
              </i>):(<span></span>)}
            </span>)}
