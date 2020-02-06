f3aeb98e607adfe2f1539ee0ec6e421c1fb022f7
aditya109 <adikid1996@gmail.com>
2020-02-06 12:46:01 +0530

    comment resolution
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- src/common/addMetrics.js
+++ src/common/addMetrics.js
@@ -2,8 +2,8 @@
 export const standardizeMetrics = (num) => {
     if (num > 1000000) {
         let suffix = "M+"
-        let k1 = ((num*1.0)/ 1000000).toFixed(1)
-        return (k1 + suffix)
+        let numeric_prefix = ((num*1.0)/ 1000000).toFixed(1)
+        return (numeric_prefix + suffix)
     } 
     else if (num > 1000) {
         let suffix = "K+"
