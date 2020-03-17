f8e295d2f87d5591c7e51dd9e47f00bacd42137c
Chandan Kumar <chandan.kumar@mayadata.io>
2020-03-17 15:02:51 +0530

    (refactor): Handle the edge cases and refactor the code (#117)
    Signed-off-by: Chandan Kumar <chandan.kr404@gmail.com>

--- src/components/ChartDetails.js
+++ src/components/ChartDetails.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react';
 import Modal from 'react-modal';
 import classNames from 'classnames';
 import { InstallModalContent } from '../components/InstallModalContent';
 import { ChartCard } from '../components/ChartCard';
 import { GoChevronDown } from 'react-icons/go';
-import { IconContext } from "react-icons";
+import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
 import { VideoModalContent } from './VideoModalContent';
-const ReactDOM = require('react-dom')
-const ReactMarkdown = require('react-markdown')
+const ReactMarkdown = require('react-markdown');
 const customStyles = {
   content: {
     top: '50%',
@@ -22,21 +21,26 @@ const customStyles = {
   }
 };
 
-
 class UsesExplanation extends React.Component {
   constructor(props) {
     super();
   }
   render() {
     return (
-      <div className="d-flex item-block">
-        <i className={this.props.classCSS}></i>  
-        <div className="d-flex flex-column items"> 
-          {this.props.displaylabel != "" ? (<span className="uses-explanation-title">{this.props.displaylabel}</span>) : ""}
+      <div className='d-flex item-block'>
+        <i className={this.props.classCSS}></i>
+        <div className='d-flex flex-column items'>
+          {this.props.displaylabel !== '' ? (
+            <span className='uses-explanation-title'>
+              {this.props.displaylabel}
+            </span>
+          ) : (
+            ''
+          )}
           {this.props.displaytext}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -55,7 +59,7 @@ export class ChartDetails extends React.Component {
     this.handleVideoCloseModal = this.handleVideoCloseModal.bind(this);
     this.host = null;
   }
-  
+
   handleCollapseContent = () => {
     const isCollapsed = this.state.isCollapsed;
     if (isCollapsed) {
@@ -63,49 +67,81 @@ export class ChartDetails extends React.Component {
     } else {
       this.setState({ isCollapsed: true });
     }
-  }
-  createLink = (listOfLink) => {
-    let div = [];
-    for (let i = 0; i < listOfLink.length; i++) {
-      div.push(
-        <a key={i} href={listOfLink[i].url}><span key={i}> {listOfLink[i].name} </span></a>
-      )
-    }
-    return div;
-  }
-
-  getMaintainerList = (listofMaintainers) => {
-    let div = [];
-    for (let i = 0; i < listofMaintainers.length; i++) {
-      div.push(<span key={i}>{listofMaintainers[i].name}</span>, <a href={listofMaintainers[i].email} key={listofMaintainers[i].email}><span key={i}> {listofMaintainers[i].email} </span></a>)
-    }
-    return div;
-  }
-  getPlatformList = (listofPlatforms) => {
-    let div = [];
-    if (listofPlatforms != null && listofPlatforms.length > 0) {
-      div.push(<span className="uses-explanation-title"> Platforms</span>)
-      for (let i = 0; i < listofPlatforms.length; i++) {
-        div.push(<span key={i}>{listofPlatforms[i]}</span>)
+  };
+  createLink = listOfLink => {
+    try {
+      let div = [];
+      if (listOfLink) {
+        for (let i = 0; i < listOfLink.length; i++) {
+          div.push(
+            <a key={i} href={listOfLink[i].url}>
+              <span key={i}> {listOfLink[i].name}</span>
+            </a>
+          );
+        }
+        return div;
       }
+    } catch (e) {
+      console.log('Catch error:', e);
     }
-    return div
-  }
-  
-  getMaturityOfExperiment = (maturityOfExperiment) => {
-    let div = [];
-    if (maturityOfExperiment != '') {
-      div.push(<span className="uses-explanation-title"> Maturity</span>)
-        div.push(<span >{maturityOfExperiment}</span>)
+  };
+
+  getMaintainerList = listofMaintainers => {
+    try {
+      if (listofMaintainers) {
+        let div = [];
+        for (let i = 0; i < listofMaintainers.length; i++) {
+          div.push(
+            <span key={i}>{listofMaintainers[i].name}</span>,
+            <a
+              href={listofMaintainers[i].email}
+              key={listofMaintainers[i].email}
+            >
+              <span key={i}> {listofMaintainers[i].email} </span>
+            </a>
+          );
+        }
+        return div;
+      }
+    } catch (e) {
+      console.log('Catch error:', e);
     }
-    return div
-  }
-  
+  };
+  getPlatformList = listofPlatforms => {
+    try {
+      if (listofPlatforms) {
+        let div = [];
+        if (listofPlatforms != null && listofPlatforms.length > 0) {
+          div.push(<span className='uses-explanation-title'> Platforms</span>);
+          for (let i = 0; i < listofPlatforms.length; i++) {
+            div.push(<span key={i}>{listofPlatforms[i]}</span>);
+          }
+        }
+        return div;
+      }
+    } catch (e) {
+      console.log('Catch error:', e);
+    }
+  };
+
+  getMaturityOfExperiment = maturityOfExperiment => {
+    try {
+      if (maturityOfExperiment !== '') {
+        let div = [];
+        div.push(<span className='uses-explanation-title'> Maturity</span>);
+        div.push(<span>{maturityOfExperiment}</span>);
+        return div;
+      }
+    } catch (e) {
+      console.log('Catch error:', e);
+    }
+  };
+
   handleOpenModal() {
     this.setState({ showModal: true });
   }
-  handleVideoOpenModal(){ 
-    this.setState({showVideoModal:true});
+  handleVideoOpenModal() {
+    this.setState({ showVideoModal: true });
   }
   handleVideoCloseModal() {
     this.setState({ showVideoModal: false });
@@ -113,66 +149,71 @@ export class ChartDetails extends React.Component {
 
   handleNavHome = () => {
     this.props.history.push('/');
-  }
-  handleNavToExperiment = (chartName,experiments) => {
+  };
+  handleNavToExperiment = (chartName, experiments) => {
     this.props.history.push(`/charts/${chartName}/experiments/${experiments}`);
-  }
+  };
 
   renderExperiments = function() {
-    let logo = "https://raw.githubusercontent.com/litmuschaos/chaos-charts/master/charts/"+this.props.charts.metadata.name+"/icons/"
-    let displayName = this.props.charts.metadata.name;
-      const experiments = this.props.charts.experiments.map(
-      chart => <Link to={`/charts/${displayName}/experiments/${chart.metadata.name}`}>
-      <ChartCard 
-      isCard='true' 
-      key={chart.metadata.name} 
-      title={chart.spec.displayName} 
-      analytics={this.props.analytics.filter(exp=>exp.Label == chart.metadata.name)}
-      chaosType={chart.spec.chaosType}
-      chartType={this.props.charts.metadata.name}
-      provider={chart.spec.provider.name}
-      text={chart.metadata.annotations.chartDescription} 
-      icon={logo + chart.metadata.name +".png"} 
-      id={chart.metadata.name} />
-      </Link>)
-    return (
-      [...experiments]
-    )
-  }
+    try {
+      let logo =
+        'https://raw.githubusercontent.com/litmuschaos/chaos-charts/master/charts/' +
+        this.props.charts.metadata.name +
+        '/icons/';
+      let displayName = this.props.charts.metadata.name;
+      const experiments = this.props.charts.experiments.map(chart => (
+        <Link to={`/charts/${displayName}/experiments/${chart.metadata.name}`}>
+          <ChartCard
+            isCard='true'
+            key={chart.metadata.name}
+            title={chart.spec.displayName}
+            analytics={this.props.analytics.filter(
+              exp => exp.Label === chart.metadata.name
+            )}
+            chaosType={chart.spec.chaosType}
+            chartType={this.props.charts.metadata.name}
+            provider={chart.spec.provider.name}
+            text={chart.metadata.annotations.chartDescription}
+            icon={logo + chart.metadata.name + '.png'}
+            id={chart.metadata.name}
+          />
+        </Link>
+      ));
+      return [...experiments];
+    } catch (e) {
+      console.log('Catch error:', e);
+    }
+  };
 
   displayLinkCreator = () => {
-    this.host = window.location.host
-    this.hostname = window.location.hostname
-    var path = this.props.charts.spec.chaosExpCRDLink
-    path = path.split("/charts/")[1]
-    var prefixPath = "https://"
-    var suffixPath = "/api/chaos?file=charts/"
-    if (this.hostname === "localhost") {
-      prefixPath = "http://"
-      this.host = "localhost:8080"
-      suffixPath = "/chaos?file=charts/"
+    try {
+      this.host = window.location.host;
+      this.hostname = window.location.hostname;
+      var path = this.props.charts.spec.chaosExpCRDLink;
+      path = path.split('/charts/')[1];
+      var prefixPath = 'https://';
+      var suffixPath = '/api/chaos?file=charts/';
+      if (this.hostname === 'localhost') {
+        prefixPath = 'http://';
+        this.host = 'localhost:8080';
+        suffixPath = '/chaos?file=charts/';
+      }
+      var displayRepoPath = prefixPath + this.host + suffixPath + path;
+      return displayRepoPath;
+    } catch (e) {
+      console.log('Catch error:', e);
     }
-    var displayRepoPath = prefixPath+this.host+suffixPath+path
-    return displayRepoPath
-  }
+  };
 
   showCards(flag) {
-    if (!flag) {
-      return null;
-    } else {
+    if (flag) {
       return (
         <div>
           <h3>Chaos Experiments</h3>
-           <div className="d-flex">
-             {this.renderExperiments()}
-          </div>
+          <div className='d-flex'>{this.renderExperiments()}</div>
         </div>
       );
     }
-  }
-
-  getFilePath() {
-    console.log(this.props.charts.spec.displayName)
   }
 
   handleCloseModal() {
@@ -180,63 +221,142 @@ export class ChartDetails extends React.Component {
   }
 
   render() {
-    const isCollapsed = classNames({
-      'chart-details-content': true,
-      'content-is-collapsed': this.state.isCollapsed,
-      'content-is-open': !this.state.isCollapsed
-    });
+    try {
+      const isCollapsed = classNames({
+        'chart-details-content': true,
+        'content-is-collapsed': this.state.isCollapsed,
+        'content-is-open': !this.state.isCollapsed
+      });
 
-    return (
-      <div className="chart-details-container">
-      <div className="chart-details-header">
-        <div className="chart-details-title-container" onClick={this.handleCollapseContent}>
-          <span className="chart-details-title">{this.props.name}</span>
-          <IconContext.Provider value={{ color: "#004ED6", size: '1.5em' }}>
-            <GoChevronDown />
-          </IconContext.Provider>
-        </div>
-        <button className="chart-install-button" onClick={this.handleOpenModal}>{this.props.install_button_text}</button>
-      </div>
-      <div className="metrics-message">{this.props.CountMessage} : {this.props.ChartCount}
-      </div>
-      <div className={isCollapsed}>
-        <div className = "chart-details-text-container">
-          <div className="chart-details-text">
-            <ReactMarkdown source={this.props.charts.spec.categoryDescription} />         
+      return (
+        <div className='chart-details-container'>
+          <div className='chart-details-header'>
+            <div
+              className='chart-details-title-container'
+              onClick={this.handleCollapseContent}
+            >
+              <span className='chart-details-title'>{this.props.name}</span>
+              <IconContext.Provider value={{ color: '#004ED6', size: '1.5em' }}>
+                <GoChevronDown />
+              </IconContext.Provider>
+            </div>
+            <button
+              className='chart-install-button'
+              onClick={this.handleOpenModal}
+            >
+              {this.props.install_button_text}
+            </button>
           </div>
-          <div className = "tutorial-video-container">
-            {this.props.video!="" && this.props.charts.experiments===null ? (<button className ="tutorial-button" onClick={this.handleVideoOpenModal}>
-              <span className = "tutorial-title">Experiment Demo</span> 
-              <div><img className="tutorial-img" src={process.env.PUBLIC_URL + '/icons/play-square.png'}/></div></button>):("")}
+          <div className='metrics-message'>
+            {this.props.CountMessage} : {this.props.ChartCount}
+          </div>
+          <div className={isCollapsed}>
+            <div className='chart-details-text-container'>
+              <div className='chart-details-text'>
+                <ReactMarkdown
+                  source={this.props.charts.spec.categoryDescription}
+                />
+              </div>
+              <div className='tutorial-video-container'>
+                {this.props.video !== '' &&
+                this.props.charts.experiments === null ? (
+                  <button
+                    className='tutorial-button'
+                    onClick={this.handleVideoOpenModal}
+                  >
+                    <span className='tutorial-title'>Experiment Demo</span>
+                    <div>
+                      <img
+                        alt='play-square'
+                        className='tutorial-img'
+                        src={process.env.PUBLIC_URL + '/icons/play-square.png'}
+                      />
+                    </div>
+                  </button>
+                ) : (
+                  ''
+                )}
+              </div>
+            </div>
+            <button
+              className='chart-install-button-phone'
+              onClick={this.handleOpenModal}
+            >
+              {this.props.install_button_text}
+            </button>
+            <div className='chart-details-uses-explanation'>
+              <UsesExplanation
+                classCSS='mi-link dark-gray'
+                displaylabel='Useful Links'
+                displaytext={this.createLink(this.props.charts.spec.links)}
+              />
+              <UsesExplanation
+                classCSS='mi-link dark-gray'
+                displaylabel='Maintainers'
+                displaytext={this.getMaintainerList(
+                  this.props.charts.spec.maintainers
+                )}
+              />
+              {this.props.charts.spec.platforms != null && (
+                <UsesExplanation
+                  classCSS='mi-container dark-gray'
+                  displaylabel=''
+                  displaytext={this.getPlatformList(
+                    this.props.charts.spec.platforms
+                  )}
+                />
+              )}
+              {this.props.charts.spec.maturity !== '' && (
+                <UsesExplanation
+                  classCSS='mi-chart-bar-up dark-gray'
+                  displaylabel=''
+                  displaytext={this.getMaturityOfExperiment(
+                    this.props.charts.spec.maturity
+                  )}
+                />
+              )}
+            </div>
+          </div>
+          <Modal
+            isOpen={this.state.showModal}
+            contentLabel='Minimal Modal Example'
+            style={customStyles}
+          >
+            <InstallModalContent
+              expcrdurl={this.displayLinkCreator()}
+              provider={this.props.charts.spec.provider.name}
+              logo={this.props.logo}
+              display
+              displayName={this.props.charts.spec.displayName}
+            />
+            <button
+              className='modal-close-button'
+              onClick={this.handleCloseModal}
+            >
+              <span className='modal-close rounded'></span>
+            </button>
+          </Modal>
+          <Modal
+            isOpen={this.state.showVideoModal}
+            contentLabel='Video Modal Example'
+            style={customStyles}
+          >
+            <VideoModalContent video={this.props.video}></VideoModalContent>
+            <button
+              className='modal-close-button'
+              onClick={this.handleVideoCloseModal}
+            >
+              <span className='modal-close rounded'></span>
+            </button>
+          </Modal>
+
+          <div>
+            {this.props.charts.experiments === null ? '' : this.showCards(true)}
           </div>
         </div>
-        <button className="chart-install-button-phone" onClick={this.handleOpenModal}>{this.props.install_button_text}</button>
-        <div className="chart-details-uses-explanation">
-          <UsesExplanation classCSS = "mi-link dark-gray" displaylabel = "Useful Links" displaytext = {this.createLink(this.props.charts.spec.links)}/>
-          <UsesExplanation classCSS = "mi-link dark-gray" displaylabel = "Maintainers" displaytext = {this.getMaintainerList(this.props.charts.spec.maintainers)}/>
-          {this.props.charts.spec.platforms != null && <UsesExplanation classCSS = "mi-container dark-gray" displaylabel = "" displaytext = {this.getPlatformList(this.props.charts.spec.platforms)}/>}
-          {this.props.charts.spec.maturity != '' &&  <UsesExplanation classCSS = "mi-chart-bar-up dark-gray" displaylabel = "" displaytext = {this.getMaturityOfExperiment(this.props.charts.spec.maturity)}/>}
-        </div>
-      </div>
-      <Modal
-        isOpen={this.state.showModal}
-        contentLabel="Minimal Modal Example"
-        style={customStyles}>
-        <InstallModalContent expcrdurl={this.displayLinkCreator()} provider={this.props.charts.spec.provider.name} logo={this.props.logo} display displayName={this.props.charts.spec.displayName} />
-        <button className="modal-close-button" onClick={this.handleCloseModal}><span className="modal-close rounded"></span></button>
-      </Modal>
-      <Modal
-      isOpen={this.state.showVideoModal}
-      contentLabel="Video Modal Example"
-      style={customStyles}>
-        <VideoModalContent
-        video={this.props.video}>
-        </VideoModalContent>
-        <button className="modal-close-button" onClick={this.handleVideoCloseModal}><span className="modal-close rounded"></span></button>
-      </Modal>
-      
-      <div>{this.props.charts.experiments===null? "":this.showCards(true)}</div>
-      </div>
-    )
+      );
+    } catch (e) {
+      console.log('Catch error:', e);
+    }
   }
 }
