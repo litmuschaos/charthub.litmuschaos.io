112a00d38f271477cc03c40c889a5882e68d2cca
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-06-04 12:05:32 +0530

    added new component for the root path
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>

--- app/client/src/pages/HomePage/index.tsx
+++ app/client/src/pages/HomePage/index.tsx
@@ -1,19 +1,19 @@
-import * as React from "react";
-import { useStyles } from "./styles";
-import { useState } from "react";
+import {
+	Button,
+	Container,
+	FormControl,
+	Icon,
+	Select,
+	Typography,
+} from "@material-ui/core";
 import InputBase from "@material-ui/core/InputBase";
 import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
 import Search from "@material-ui/icons/SearchRounded";
 import Sort from "@material-ui/icons/Sort";
-import {
-	FormControl,
-	Icon,
-	Select,
-	Typography,
-	Container,
-	Button,
-} from "@material-ui/core";
+import * as React from "react";
+import { useState } from "react";
+import { useStyles } from "./styles";
 
 function HomePage() {
 	const classes = useStyles();
