6f52b1b392dbb7a2b8efcc5d714e4e48464b8306
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-06-04 15:40:31 +0530

    Added the color theme as per design
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>

--- app/client/src/withTheme.tsx
+++ app/client/src/withTheme.tsx
@@ -1,108 +1,76 @@
 import { CssBaseline } from "@material-ui/core";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { Breakpoint } from "@material-ui/core/styles/createBreakpoints";
 import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
 import * as React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "./redux/reducers";
 
 // Agument the Theme interface
 declare module "@material-ui/core/styles/createMuiTheme" {
 	interface Theme {
-		appDrawer: {
-			width: React.CSSProperties["width"];
-			breakpoint: Breakpoint;
-		};
+		// newProperty: {
+		// 	key: value;
+		// }
 	}
 	// allow configuration using `createMuiTheme`
 	interface ThemeOptions {
-		appDrawer?: {
-			width?: React.CSSProperties["width"];
-			breakpoint?: Breakpoint;
-		};
+		// newProperty?: {
+		// 	key?: value;
+		// }
 	}
 }
 
 // Augument the Palette interface
 declare module "@material-ui/core/styles/createPalette" {
 	interface Palette {
-		tertiary: {
-			main: string;
-		};
+		// newProperty: {
+		// 	key: value;
+		// }
 	}
 	// allow configuration using `createMuiTheme`
 	interface PaletteOptions {
-		tertiary?: {
-			main?: string;
-		};
+		// newProperty?: {
+		// 	key?: value;
+		// }
 	}
 }
 function customTheme(options: ThemeOptions) {
 	return createMuiTheme({
-		appDrawer: {
-			width: 225,
-			breakpoint: "lg",
-		},
 		palette: {
 			primary: {
-				light: "#fff",
-				main: "#fafafa",
-				dark: "#363839",
-				contrastText: "#000",
+				light: "rgba(225, 226, 238, 0.88)",
+				main: "rgba(225, 226, 238)",
+				dark: "rgb(204, 208, 239)",
+				contrastText: "rgba(20, 47, 72, 0.95)",
 			},
 			secondary: {
-				light: "#ff5e50",
-				main: "#e41e26",
-				dark: "#a90000",
-				contrastText: "#fff",
+				light: "rgb(10, 55, 171)",
+				main: "rgb(54, 220, 200)",
+				dark: "rgba(20, 47, 72, 0.88)",
+				contrastText: "rgb(10, 55, 171)",
 			},
 			background: {
-				paper: "#fafafa",
-				default: "#fafafa",
+				paper: "rgba(225, 226, 238)",
+				default: "rgb(255, 255, 255)",
 			},
 			text: {
-				primary: "#000",
-			},
-			tertiary: {
-				main: "#ffd900",
+				primary: "rgba(20, 47, 72, 0.72)",
+				secondary: "rgba(20, 47, 72, 0.88)",
+				disabled: "rgba(20, 47, 72, 0.54)",
+				hint: "rgba(20, 47, 72, 0.54)",
 			},
 		},
 		...options,
 	});
 }
 
-const lightTheme = customTheme({});
-
-const darktheme = customTheme({
-	palette: {
-		primary: {
-			light: "#323232",
-			main: "#2C2C2C",
-			dark: "#121212",
-			contrastText: "#fff",
-		},
-		background: {
-			paper: "#323232",
-			default: "#222222",
-		},
-		text: {
-			primary: "#fff",
-		},
-		tertiary: {
-			main: "#0080ff",
-		},
-	},
-});
+const theme = customTheme({});
 
 export function withTheme(Component: any) {
 	function WithTheme(props: object) {
 		// MuiThemeProvider makes the theme available down the React tree
 		// thanks to React context.
-		const isDarkTheme = useSelector(
-			(state: RootState) => state.theme.isDarkTheme
-		);
+
 		return (
-			<ThemeProvider theme={!isDarkTheme ? lightTheme : darktheme}>
+			<ThemeProvider theme={theme}>
 				{/* Reboot kickstart an elegant, consistent, and simple baseline to build upon. */}
 				<CssBaseline />
 				<Component {...props} />
