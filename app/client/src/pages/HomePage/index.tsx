e02d2c71737c3b5067493221aed4ed828222b619
Amit Kumar Das <40661238+amityt@users.noreply.github.com>
2020-06-05 17:32:31 +0530

    Added title components for homepage (#4)
    Signed-off-by: Amit Kumar Das <amitkumar.das@mayadata.io>

--- app/client/src/pages/HomePage/index.tsx
+++ app/client/src/pages/HomePage/index.tsx
@@ -1,15 +1,137 @@
-import { Typography } from "@material-ui/core";
 import * as React from "react";
 import { useStyles } from "./styles";
+import { useState } from "react";
+import InputBase from "@material-ui/core/InputBase";
+import MenuItem from "@material-ui/core/MenuItem";
+import Paper from "@material-ui/core/Paper";
+import Search from "@material-ui/icons/SearchRounded";
+import Sort from "@material-ui/icons/Sort";
+import {
+	FormControl,
+	Icon,
+	Select,
+	Typography,
+	Container,
+	Button,
+} from "@material-ui/core";
 
 function HomePage() {
 	const classes = useStyles();
-
+	const [selectChaos, setSelectChaos] = useState("All");
+	const [selectContributors, setSelectContributors] = useState("All");
+	const handleChaosChange = (
+		event: React.ChangeEvent<{ value: unknown }>
+	) => {
+		setSelectChaos(event.target.value as string);
+	};
+	const handleContributorChange = (
+		event: React.ChangeEvent<{ value: unknown }>
+	) => {
+		setSelectContributors(event.target.value as string);
+	};
+	const handleSort = () => {
+		console.log("Sort button active!");
+	};
+	const [search, setSearch] = useState();
 	return (
 		<div className={classes.root}>
-			<Typography variant="h4" gutterBottom>
-				Home Page
+			<Typography variant="h4" style={{ fontSize: "40px" }} gutterBottom>
+				<b>Chaos Chart for Kubernetes</b>
 			</Typography>
+			<Typography variant="subtitle1" className={classes.description}>
+				Charts are pre-defined chaos experiments. Use these charts to
+				inject chaos into cloud native applications and Kubernetes
+				infrastructure.
+			</Typography>
+			<Typography variant="h6" className={classes.description1}>
+				Browse . Run . Contribute
+			</Typography>
+
+			<Paper
+				component="form"
+				className={classes.searchField}
+				onSubmit={(e) => {
+					e.preventDefault();
+				}}
+			>
+				<Icon className={classes.searchIcon} aria-label="menu">
+					<Search />
+				</Icon>
+				<InputBase
+					className={classes.textField}
+					placeholder="Search for Chaos Experiments"
+					inputProps={{
+						"aria-label": "search for chaos experiments",
+					}}
+				/>
+			</Paper>
+			<Container maxWidth="lg">
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						marginTop: 50,
+						marginBottom: 20,
+					}}
+				>
+					<FormControl className={classes.formControl}>
+						<Typography style={{ float: "left", marginTop: 2 }}>
+							Chaos for :
+						</Typography>
+						<Select
+							className={classes.selectOption}
+							disableUnderline={true}
+							labelId="change-chaos"
+							value={selectChaos}
+							onChange={handleChaosChange}
+						>
+							<MenuItem value={"All"}>All</MenuItem>
+							<MenuItem value={"Cassandra"}>Cassandra</MenuItem>
+							<MenuItem value={"Kubernetes"}>Kubernetes</MenuItem>
+							<MenuItem value={"Kafka"}>Kafka</MenuItem>
+							<MenuItem value={"OpenEBS"}>OpenEBS</MenuItem>
+						</Select>
+					</FormControl>
+
+					<FormControl className={classes.formControl}>
+						<Typography
+							style={{
+								float: "left",
+								marginTop: 2,
+								marginLeft: 80,
+							}}
+						>
+							Contributors :
+						</Typography>
+						<Select
+							className={classes.selectOption}
+							disableUnderline={true}
+							labelId="change-contributors"
+							value={selectContributors}
+							onChange={handleContributorChange}
+						>
+							<MenuItem value={"All"}>All</MenuItem>
+							<MenuItem value={"MayaData"}>MayaData</MenuItem>
+							<MenuItem value={"Intuit"}>Intuit</MenuItem>
+						</Select>
+					</FormControl>
+
+					<Button
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							marginLeft: "auto",
+							borderRadius: 4,
+						}}
+						onClick={handleSort}
+					>
+						<Icon style={{ marginBottom: 10 }}>
+							<Sort />
+						</Icon>
+						<Typography className={classes.sort}>Sort</Typography>
+					</Button>
+				</div>
+			</Container>
 		</div>
 	);
 }
