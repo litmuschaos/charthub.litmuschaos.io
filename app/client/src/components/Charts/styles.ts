c786ed0d1c3d1398471a044c077999043a466e37
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-06-09 00:22:09 +0530

    added basic styling to the cards and commented out analytics data
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>

--- app/client/src/components/Charts/styles.ts
+++ app/client/src/components/Charts/styles.ts
@@ -4,9 +4,9 @@ export const useStyles = makeStyles((theme: Theme) => ({
 	root: {
 		width: "100%",
 		display: "flex",
-	},
-	spinner: {
-		margin: "0 auto",
-		color: theme.palette.secondary.dark,
+		flexDirection: "column",
+		[theme.breakpoints.up("md")]: {
+			flexDirection: "row",
+		},
 	},
 }));
