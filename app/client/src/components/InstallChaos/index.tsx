337b5ce69627dc262ae7583120be57633f55832a
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-06-15 13:30:03 +0530

    added yaml text for engine rbac
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>

--- app/client/src/components/InstallChaos/index.tsx
+++ app/client/src/components/InstallChaos/index.tsx
@@ -1,21 +1,33 @@
 import { IconButton } from "@material-ui/core";
 import Done from "@material-ui/icons/DoneAllTwoTone";
 import Copy from "@material-ui/icons/FileCopyTwoTone";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useStyles } from "./styles";
 
 interface InstallProps {
 	title: string;
 	description: string;
 	yamlLink: string;
+	engine: boolean;
 }
 
 export function InstallChaos(props: InstallProps) {
 	const classes = useStyles();
+	const { title, description, yamlLink, engine } = props;
 	const [copying, setCopying] = useState(false);
+	const [yaml, setYaml] = useState(`kubectl apply -f ${yamlLink}`);
 
-	const { title, description, yamlLink } = props;
-	const yamlCommand = `kubectl apply -f ${yamlLink}`;
+	function fetchYaml(yamlLink: string) {
+		fetch(yamlLink)
+			.then((data) => {
+				data.text().then((yamlText) => {
+					setYaml(yamlText);
+				});
+			})
+			.catch((err) => {
+				console.error("Unable to fetch the yaml text" + err);
+			});
+	}
 
 	function copyTextToClipboard(text: string) {
 		if (!navigator.clipboard) {
@@ -32,16 +44,19 @@ export function InstallChaos(props: InstallProps) {
 
 		setTimeout(() => setCopying(false), 3000);
 	}
+
+	useEffect(() => {
+		if (engine) fetchYaml(yamlLink);
+	}, []);
+
 	return (
 		<div className={classes.root}>
 			<div className={classes.title}>{title}</div>
 			<div className={classes.description}>{description}</div>
 			<div className={classes.linkBox}>
-				<div className={classes.yamlLink}>{yamlCommand}</div>
+				<div className={classes.yamlLink}>{yaml}</div>
 				<div>
-					<IconButton
-						onClick={() => copyTextToClipboard(yamlCommand)}
-					>
+					<IconButton onClick={() => copyTextToClipboard(yaml)}>
 						{!copying ? (
 							<Copy />
 						) : (
