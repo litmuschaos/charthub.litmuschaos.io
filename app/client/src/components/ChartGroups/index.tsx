05650945ecbd8b138a084f062cec8918bf5ff596
Arkajyoti Mukherjee <32966391+arkajyotiMukherjee@users.noreply.github.com>
2020-06-16 12:46:43 +0530

    Fixes1.5 (#178)
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>
    Co-authored-by: gdsoumya <gdsoumya@gmail.com>

--- app/client/src/components/ChartGroups/index.tsx
+++ app/client/src/components/ChartGroups/index.tsx
@@ -32,29 +32,35 @@ const getIconUrl = (chartMetadataName: string) => {
 export function ChartGroups(props: ChartGroupProps) {
 	const { experimentGroups } = props;
 	const classes = useStyles();
-	const analyticsData = useSelector(
-		(state: RootState) => state.analyticsData
+	const { analyticsData, versionData } = useSelector(
+		(state: RootState) => state
 	);
 
 	return (
 		<div className={classes.root}>
 			{experimentGroups &&
-				experimentGroups.map((g: ExperimentGroup) => (
-					<CustomCard
-						key={g.metadataName}
-						id={g.metadataName}
-						title={g.name}
-						urlToIcon={getIconUrl(g.metadataName)}
-						handleClick={() => history.push(`/${g.metadataName}`)}
-						experimentCount={g.experiments.length}
-						provider={g.provider}
-						description={g.description}
-						totalRuns={getExpRunCount(
-							g.experiments,
-							analyticsData.expAnalytics
-						)}
-					/>
-				))}
+				experimentGroups.map(
+					(g: ExperimentGroup) =>
+						(g.experiments.length ||
+							versionData.currentVersion === "master") && (
+							<CustomCard
+								key={g.metadataName}
+								id={g.metadataName}
+								title={g.name}
+								urlToIcon={getIconUrl(g.metadataName)}
+								handleClick={() =>
+									history.push(`/${g.metadataName}`)
+								}
+								experimentCount={g.experiments.length}
+								provider={g.provider}
+								description={g.description}
+								totalRuns={getExpRunCount(
+									g.experiments,
+									analyticsData.expAnalytics
+								)}
+							/>
+						)
+				)}
 		</div>
 	);
 }
