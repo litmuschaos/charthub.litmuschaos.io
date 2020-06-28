d88b9f6c6cb50e6578eb69e950d5ee66448c16a8
Arkajyoti Mukherjee <32966391+arkajyotiMukherjee@users.noreply.github.com>
2020-06-28 07:12:29 +0530

    Changed the Footer, Made the mobile view better, added menu button for drawer in mobile, made some css changes to make it more responsive (#187)
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>

--- app/client/src/redux/model/index.ts
+++ app/client/src/redux/model/index.ts
@@ -1,17 +1,14 @@
 import { AnalyticsAction } from "./analytics";
 import { ChartAction } from "./charts";
 import { GithubAction } from "./github";
-import { ThemeAction } from "./theme";
 import { VersionAction } from "./versions";
 
 export * from "./analytics";
 export * from "./charts";
 export * from "./github";
-export * from "./theme";
 export * from "./versions";
 
 export type Action =
-	| ThemeAction
 	| ChartAction
 	| AnalyticsAction
 	| VersionAction
--- app/client/src/redux/reducers/index.ts
+++ app/client/src/redux/reducers/index.ts
@@ -1,20 +1,12 @@
 import { History } from "history";
 import { combineReducers } from "redux";
-import {
-	AnalyticsData,
-	ChartData,
-	GithubData,
-	Theme,
-	VersionData,
-} from "../model";
+import { AnalyticsData, ChartData, GithubData, VersionData } from "../model";
 import * as analyticsReducer from "./analytics";
 import * as chartReducer from "./charts";
 import * as githubReducer from "./github";
-import * as themeReducer from "./theme";
 import * as versionsReducer from "./versions";
 
 export interface RootState {
-	theme: Theme;
 	githubData: GithubData;
 	analyticsData: AnalyticsData;
 	versionData: VersionData;
@@ -23,7 +15,6 @@ export interface RootState {
 
 export default (history: History) =>
 	combineReducers({
-		...themeReducer,
 		...githubReducer,
 		...analyticsReducer,
 		...versionsReducer,
