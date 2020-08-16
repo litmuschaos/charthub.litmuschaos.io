266bd6a9d88d5a6ea1dc3f14da03a940df9b7132
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2020-08-16 19:17:57 +0530

    Updated Header Description
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- app/client/src/pages/HomePage/index.tsx
+++ app/client/src/pages/HomePage/index.tsx
@@ -66,10 +66,11 @@ function HomePage() {
 									Chaos Charts for Kubernetes
 								</Typography>
 								<Typography className={classes.headerDesc}>
-									Charts are pre-defined chaos experiments.
-									Use these charts to inject chaos into cloud
-									native applications and Kubernetes
-									infrastructure.
+									Litmus ChaosHub hosts chaos experiments for
+									Kubernetes. The experiments are declarative
+									and tunable. Use the hub interface to tune
+									them to your needs, deploy them and take
+									that step towards resilience.
 								</Typography>
 							</div>
 							<Hidden smDown>
