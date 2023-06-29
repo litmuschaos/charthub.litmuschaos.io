df2529b9bc67458bf1579626ed28478a14ae1283
Amit Kumar Das <amit.das@harness.io>
2023-06-29 11:37:07 +0530

    minor revert
    Signed-off-by: Amit Kumar Das <amit.das@harness.io>

--- app/server/main.go
+++ app/server/main.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/litmuschaos/charthub.litmuschaos.io/app/server/pkg/analytics"
 	"github.com/litmuschaos/charthub.litmuschaos.io/app/server/pkg/community"
+	"github.com/litmuschaos/charthub.litmuschaos.io/app/server/pkg/github"
+	"github.com/litmuschaos/charthub.litmuschaos.io/app/server/pkg/gitops"
 	"github.com/litmuschaos/charthub.litmuschaos.io/app/server/routes"
 )
 
 func main() {
 	// Handler is go-routine which synchronously calls the git-ops function UpdateAnalyticsData()
-	// go analytics.Handler()
-	// // Trigger is go-routine which synchronously calls the git-ops function Trigger()
-	// go gitops.Trigger()
-	// // Handler is go-routine which synchronously calls the github function UpdateGithubData()
-	// go github.Handler()
+	go analytics.Handler()
+	// Trigger is go-routine which synchronously calls the git-ops function Trigger()
+	go gitops.Trigger()
+	// Handler is go-routine which synchronously calls the github function UpdateGithubData()
+	go github.Handler()
 	// Starts all handlers for community analytics
 	community.StartHandlers()
 	router := routes.NewRouter()
